dc97c94fad13ac5cfd1ebe24e45efda69081db6c
BratanaCodeX <ivanovivanae.17@gmail.com>
2025-02-10 23:57:27 +0200

    Cleaned untracked files and updated .gitignore

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1 className="text-center text-2xl font-bold mt-4">
+        Добре дошли в BratanaVPN!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
